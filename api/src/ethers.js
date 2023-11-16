9a3f6780b72d672f7e7a1b7df2424867e3b7c00d
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-11-16 16:15:22 +0530

    ✨ | added api

---
+++ api/src/ethers.js
@@ -0,0 +1,21 @@
+import { Contract, JsonRpcProvider, Wallet } from "ethers";
+import details from "./artifacts/details.json" assert { type: "json" };
+import Cert from "./artifacts/contracts/Cert.sol/Cert.json" assert { type: "json" };
+
+let instance;
+
+if (process.env.CHAIN === "sepolia") {
+  const provider = new JsonRpcProvider(
+    `https://eth-sepolia.g.alchemy.com/v2/${process.env.API_KEY}`,
+  );
+  const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
+
+  instance = new Contract(details.contract, Cert.abi, wallet);
+} else {
+  const provider = new JsonRpcProvider("http://127.0.0.1:8545");
+  const signer = await provider.getSigner(details.deployer);
+
+  instance = new Contract(details.contract, Cert.abi, signer);
+}
+
+export default instance;
---
+++ api/src/routes/index.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import instance from "../ethers.js";
+const router = Router();
+
+router.post("/issue", async function (req, res) {
+  let body = req.body;
+  console.log(body);
+
+  try {
+    const trx = await instance.issue(
+      body.id,
+      body.name,
+      body.course,
+      body.grade,
+      body.date,
+    );
+    res.status(201).json(trx);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/fetch", async function (req, res) {
+  let query = req.query;
+  console.log(query);
+
+  try {
+    const result = await instance.Certificates(query.id);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+export default router;
